cc8ea7438a95d89afa6c169e49f671cf296ecb77
Anusha9Solanky <129885590+Anusha9Solanky@users.noreply.github.com>
2023-08-14 21:16:53 +0530

    Create BMI.java

---
+++ BMI.java
@@ -0,0 +1,22 @@
+package BMI;
+import java.util.Scanner;
+public class BMI_CALCULATOR {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Weight in Kilograms: ");
+		double weight = sc.nextDouble();
+		System.out.println("Height in Meters: ");
+		double height = sc.nextDouble();
+		double BMI = weight/(height*height);
+		System.out.println(String.format("%.3f", BMI));
+		if (BMI < 18.5)
+			System.out.println ("Underweight");
+		else if (BMI < 25 )
+			System.out.println("Normal");
+		else if (BMI < 30)
+			System.out.println("Overweight");
+		else 
+			System.out.println("Obese");
+		}
+
+}
